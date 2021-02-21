44f792fbb510d7eef36811c2da599a3ea1e3edb7
Kai Dong <dongkai981@gmail.com>
2021-02-21 11:33:33 -0600

    Updated server.js with MONGODB_URI variable, added .env and .gitignore

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config();
 
 const PORT = 8080;
 
@@ -15,9 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true
 });
 
 // routes
